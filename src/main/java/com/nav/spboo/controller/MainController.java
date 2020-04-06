c867ac33b864077f4c28ceaf4e037c00f3adeec5
Vadim <makakimojo@gmail.com>
2020-04-06 22:01:00 +0300

    add dev.properties

--- src/main/java/com/nav/spboo/controller/MainController.java
+++ src/main/java/com/nav/spboo/controller/MainController.java
@@ -1,10 +1,16 @@
 package com.nav.spboo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
 
     @RequestMapping("/main")
     public String goMain() {
